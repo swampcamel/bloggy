7df56faf5dc5638e72aed228518c4a3a1f6f6487
Dan Mace <dmacebeta@gmail.com>
2018-10-02 07:13:09 -0700

    add testing tools

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "eslint-loader"
       }
     ]
   }
